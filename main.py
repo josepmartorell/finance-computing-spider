5e282258760a33939cf0bc4f17d30b2107f72756
josepmartorell <martorelljosep>
2020-06-27 22:13:27 +0200

    update

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ wikiResponse = requests.get(wikiURL)
 data = {"Company": []}
 i = 1
 
-for ticker in range(5):  # 10 instead of 500 for testing purposes
+for ticker in range(10):  # 10 instead of 500 for testing purposes
     wikiSplit = wikiResponse.text.split("external text")[i]
     wikiSplitAgain = wikiSplit.split(">")[1]
     wikiTicker = wikiSplitAgain.split("<")[0]
@@ -42,17 +42,28 @@ for tickerSymbol in data["Company"]:
     htmlText = response.text
     for indicator in Indicators:
         try:
-            yahooSplit = htmlText.split(indicator)[1]
-            yahooSplitAgain = yahooSplit.split('</span>')[1]
-            yahooSplitAnother = yahooSplitAgain.split("reactid=")[2]
-            yahooIndicator = yahooSplitAnother.split("\">")[1]
-            Indicators[indicator].append(yahooIndicator)
+            if indicator == "Day&#x27;s Range" or indicator == "52 Week Range" \
+                    or indicator == "Dividend &amp; Yield":
+                yahooSplit1 = htmlText.split(indicator)[1]
+                yahooSplit2 = yahooSplit1.split('</span>')[1]
+                yahooSplit3 = yahooSplit2.split("reactid=")[1]
+                yahooSplit4 = yahooSplit3.split("</td>")[0]
+                yahooSplit5 = yahooSplit4.split("\">")[1]
+                Indicators[indicator].append(yahooSplit5)
+            else:
+                yahooSplit = htmlText.split(indicator)[1]
+                yahooSplitAgain = yahooSplit.split('</span>')[1]
+                yahooSplitAnother = yahooSplitAgain.split("reactid=")[2]
+                yahooIndicator = yahooSplitAnother.split("\">")[1]
+                Indicators[indicator].append(yahooIndicator)
         except:
             Indicators[indicator].append("N/A")
 
 data.update(Indicators)
+pd.options.display.max_columns = None
 df = pd.DataFrame(data)
-print(df.head())
-print(df.tail())
+# print(df.head())
+# print(df.tail())
+print(df)
 
 # todo: in development...
